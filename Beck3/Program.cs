cb0470ed6f1272d2a8d3716959c65d16466e3963
cbcbeck <Hebrews43@gmail.com>
2016-07-25 06:50:56 -0400

    first commit

---
+++ Beck3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beck3
+{
+    class StringOperations
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: "); 
+            string fname = Console.ReadLine(); // 
+            Console.WriteLine("Please enter your last name: "); 
+            string lname = Console.ReadLine(); 
+            string fullname = StringOperations.CapitalizeName(fname, lname); 
+            Console.WriteLine("Your full name with first letters capitalized is: " + fullname);
+            Console.ReadKey();
+        }
+        public static string CapitalizeName(string fname, string lname)
+        {
+            string cfname = fname.First().ToString().ToUpper() + fname.Substring(1);
+            string clname = lname.First().ToString().ToUpper() + lname.Substring(1);
+            string cfullname = cfname + " " + clname;
+            return cfullname;
+        }
+
+
+
+    }
+}
